40818f33896ce701a934657ae67f03f3f175888a
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2021-09-30 14:28:14 +0300

    Adding structured logging

--- handler.js
+++ handler.js
@@ -3,6 +3,8 @@
 const AWS = require("aws-sdk");
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
+const { metricScope } = require("aws-embedded-metrics");
+
 const TABLE_NAME = process.env.TABLE_NAME;
 
 exports.saveHello = async (event) => {
@@ -15,38 +17,50 @@ exports.saveHello = async (event) => {
   };
 
   console.log(item);
+  
   const savedItem = await saveItem(item);
 
   return getResponse(savedItem);
 };
 
-exports.getHello = async (event) => {
-  const name = event.queryStringParameters.name;
+exports.getHello = metricScope(metrics =>
+    async (event) => {
+        metrics.setNamespace('gretter')
 
-  try {
-    const item = await getItem(name);
-    console.log(item);
+        const name = event.queryStringParameters.name;
 
-    if (item.date) {
-      const d = new Date(item.date);
+        try {
+            const item = await getItem(name);
+        
+            if (item.date) {
+                const d = new Date(item.date);
 
-      const message = `Was greeted on ${d.getDate()}/${
-        d.getMonth() + 1
-      }/${d.getFullYear()}`;
+                const message = `Was greeted on ${d.getDate()}/${
+                    d.getMonth() + 1
+                }/${d.getFullYear()}`;
 
-      return getResponse(message);
+                metrics.putDimensions({Service: "GetName"});
+                metrics.putMetric("GetName", 1, "put");
+                metrics.setProperty("Name", name);
+                metrics.setProperty("Result", item);
+
+                return getResponse(message);
+            }
+        } catch (e) {
+            console.log(e);
+
+            const message = "Nobody was greeted with that name";
+            
+            metrics.putDimensions({Service: "GetName"});
+            metrics.putMetric("NotFound", 1, "notFound");
+            metrics.setProperty("Name", name);
+
+            return getResponse(message);
+        }
     }
-  } catch (e) {
-    console.log(e);
-
-    const message = "Nobody was greeted with that name";
-    return getResponse(message);
-  }
-};
+);
 
 async function getItem(name) {
-  console.log("getItem");
-
   const params = {
     Key: {
       id: name,
@@ -54,13 +68,10 @@ async function getItem(name) {
     TableName: TABLE_NAME,
   };
 
-  console.log(params);
-
   return dynamo
     .get(params)
     .promise()
     .then((result) => {
-      console.log(result);
       return result.Item;
     });
 }
